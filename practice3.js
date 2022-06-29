a8358bec12dced7f2fad52398a6f012a38835598
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-06-29 13:36:33 +0530

    third commit

---
+++ practice3.js
@@ -0,0 +1,7 @@
+//JavaScript strings and operators
+var test = "Learning Javascript in Internet";
+console.log(test.length);
+console.log(test.slice(25,35));
+console.log(test.replace("Javascript", "web3.0"));
+console.log(test.toUpperCase());
+console.log(test.concat("web3.0 is future"));
