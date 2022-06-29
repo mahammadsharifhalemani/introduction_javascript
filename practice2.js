208ae783efb49d317c6d118891aff414339bb8ef
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-06-29 13:28:59 +0530

    second commit

---
+++ practice2.js
@@ -0,0 +1,15 @@
+//javascript operators
+var a = 5;
+var b = 10;
+add = a + b;
+sub = b - a;
+mult = a*b;
+div = a/b;
+inc = ++a;
+dec = --b;
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(inc);
+console.log(dec);
